4ca2641b612d784e2e6f01fe6763e067ca56fa97
neastman018 <neastman018@gmail.com>
2024-02-25 08:54:00 -0500

    Added Alarm

---
+++ backend/alarm/alarm.py
@@ -0,0 +1,84 @@
+"""
+Methods for setting and activating an alarm
+"""
+import multiprocessing
+import pygame
+from datetime import datetime 
+import string
+
+"""
+Function to inteperate time 
+"""
+#def interperate_time(time):
+
+"""
+Method to initalize the alarm
+@parameter alarm_sound: music file to play
+"""
+
+"""
+Method to play the alarm
+@parameter alarm: return of the init_alarm function which is a initalized alarm with a song loaded
+@parameter alarm_time_hour: hour the alarm should go off (24 hour time)
+@parameter alarm_time_minute: minute the alarm should go off
+@parameter alarm_time_second: minute the alarm should go off (will be 00 by default)
+@parameter stop: boolean value to stop the alarm
+"""
+
+class Alarm:
+
+    def __init__(self, alarm = pygame.mixer.music, stop = False, active = False, hour = 0, minute = 0, second = 1, day = 0, month = 0, playing = False):
+        self.alarm = alarm
+        self.stop = stop
+        self.active = active
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+        self.day = day
+        self.month = month
+        self.playing = playing
+
+        
+
+
+    def init_alarm(self, alarm_sound):
+        self.alarm.load("alarm/music/" + alarm_sound)
+
+        print(f"Alarm playing {alarm_sound} is ready")
+    
+    def is_active(self) -> bool:
+        return self.alarm.get_busy()
+        
+
+    def play_alarm(self) -> bool:
+        now = datetime.now()
+        #now.second does not register 0
+        if self.second == 0:
+            self.second = 1
+
+        if self.hour == now.hour and self.minute == now.minute and self.second == now.second and self.day == now.day and self.month == now.month and not self.is_active():
+            print(self.is_active())
+            self.playing = True
+            self.alarm.play()
+
+        if self.stop and self.is_active():
+            print("alarm is stopping")
+            self.playing = False
+            self.alarm.pause()
+
+        return self.playing
+    
+    def alarm_stop(self, stop):
+        self.stop = stop
+        return stop
+
+    if __name__ == "__main__":
+        now = datetime.now()
+        print(now.day)
+
+
+
+
+          
+           
+    
--- backend/run.py
+++ backend/run.py
@@ -3,12 +3,15 @@ import time
 from lights import LEDs
 from button.button import Button
 import RPi.GPIO as GPIO
-from subprocess import run
+from datetime import datetime
+import pygame
+from alarm.alarm import Alarm
 
 
 button1 = Button(pin=14)
 button2 = Button(pin=15)
 button3= Button(pin=17)
+alarm = Alarm(hour=8, minute=55, second=0, day = datetime.day(), month = datetime.month() )
 leds = LEDs(150, 0.5)
 strip = leds.init_leds()
 
@@ -17,25 +20,52 @@ button1.init_button()
 button2.init_button()
 button3.init_button()
 
-# state = {OFF, LED_RED, LED_GREEN}
+# state = {OFF, LED_RED, LED_GREEN, ALARM_ACTIVE}
 state = "OFF"
+pygame.mixer.init()
+alarm.init_alarm("Good_MorningV2.mp3")
+
+
+
 
 while True:
-    if button1.button_press():
-        state = "OFF"
-        print("OFF")
-        leds.off(strip)
-        #run("WAYLAND_DISPLAY='wayland-1' wlr-randr --output HDMI-A-1 --off", shell=True)
-    elif button2.button_press():
-        state = "RED"
-        print("RED")
-        #run("WAYLAND_DISPLAY='wayland-1' wlr-randr --output HDMI-A-1 --on", shell=True)
-        leds.display_color(strip, 255, 0, 0)
-    elif button3.button_press():
-        state = "GREEN"
-        print("GREEN")
-        #run("WAYLAND_DISPLAY='wayland-1' wlr-randr --output HDMI-A-1 --on", shell=True)
-        leds.display_color(strip, 0, 255, 0)
+    now = datetime.now()
+
+    if state == "ALARM_ACTIVE":
+        if button1.button_press():
+            state == "AlARM_OFF"
+            alarm.alarm_stop(True)
+
+    if state == "ALARM_OFF":    
+        alarm.play_alarm()
+        if alarm.is_active():
+            state = "ALARM_ACTIVE" 
+            leds.display_color(strip, 255, 255, 255)
+   
+        elif button1.button_press():
+            print("OFF")
+            leds.off(strip)
+        elif button2.button_press():
+            print("RED")
+            leds.display_color(strip, 255, 0, 0)
+        elif button3.button_press():
+            print("GREEN")
+            leds.display_color(strip, 0, 255, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # if state == "OFF":
     #     if button1.button_press():
     #         state = "OFF"
