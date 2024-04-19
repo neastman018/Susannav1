85102353ac3c1dfe124fb154b5fd210265163033
neastman018 <neastman018@gmail.com>
2024-04-19 10:45:35 -0400

    f

--- backend/speakertest.py
+++ backend/speakertest.py
@@ -10,32 +10,29 @@ state = False
 buttonPin = 10
 GPIO.setwarnings(False)                                        
 GPIO.setmode(GPIO.BOARD)                                        
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)    
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)   
+
+pygame.mixer.init()
+alarm = pygame.mixer.music
+alarm.load('backend/alarm/music/Good_MorningV2.mp3')      
 
 button1 = Button(buttonPin)
 button1.init_button()
 
-if __name__ == '__main__':
-    pygame.mixer.init()
-    alarm = pygame.mixer.music
-    alarm.load('alarm/music/Good_MorningV2.mp3')      
-
-    GPIO.setwarnings(False)                                        
-    GPIO.setmode(GPIO.BOARD)                                        
-    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)    
-    print("All Good in the Hood\n")
-
-    while True:
-        if button1.press() and not state:
-            alarm.play()
-            state = True
-            print("Button is Pressed, Music should be playing\n")
-
-        elif button1.press() and state:
-            alarm.stop()
-            state = False
-            print("Button is Pressed, Music should stop playing\n")
 
 
-            
-    
+print("All Good in the Hood\n")
+
+while True:
+    if button1.press() and not state:
+        alarm.play()
+        state = True
+        print("Button is Pressed, Music should be playing\n")
+
+    elif button1.press() and state:
+        #alarm.stop()
+        state = False
+        print("Button is Pressed, Music should stop playing\n")
+
+
+        
